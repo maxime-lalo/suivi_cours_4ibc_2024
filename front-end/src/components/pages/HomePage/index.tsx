6fe32258f40dfc9b319e7dbc4ca84a0c4b65f21b
Maxime Lalo <maxime.lalo.pro@gmail.com>
2024-07-10 16:09:25 +0200

    :sparkles: Creating page template

---
+++ front-end/src/components/pages/HomePage/index.tsx
@@ -0,0 +1,5 @@
+import PageTemplate from "../PageTemplate";
+
+export default function HomePage() {
+  return <PageTemplate tabTitle="Home">Home</PageTemplate>;
+}
---
+++ front-end/src/components/pages/PageTemplate/index.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import classes from "./classes.module.scss";
+
+type IProps = {
+  children?: React.ReactNode;
+  tabTitle?: string;
+};
+
+// npm i react-router-dom
+export default function PageTemplate(props: IProps) {
+  useEffect(() => {
+    document.title = props.tabTitle || "React App";
+  }, [props.tabTitle]);
+
+  return (
+    <div className={classes["root"]}>
+      <header className={"menu"}>{/*notre menu */}</header>
+      <main className={classes["body"]}>
+        <div className={classes["content"]}>{props.children}</div>
+      </main>
+    </div>
+  );
+}
