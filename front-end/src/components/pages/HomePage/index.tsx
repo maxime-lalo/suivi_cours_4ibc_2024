a8d348b5ec8c563bb9a024d745633703240f1733
Maxime Lalo <maxime.lalo.pro@gmail.com>
2024-07-10 22:38:19 +0200

    :sparkles: Styling table

--- front-end/src/components/pages/HomePage/index.tsx
+++ front-end/src/components/pages/HomePage/index.tsx
@@ -1,5 +1,42 @@
+import Table, { ECellType } from "../../elements/Table";
 import PageTemplate from "../PageTemplate";
 
 export default function HomePage() {
-  return <PageTemplate tabTitle="Home">Home</PageTemplate>;
+    return (
+        <PageTemplate tabTitle="Home">
+            <Table
+                tableHeads={[
+                    {
+                        cellType: ECellType.EURO,
+                        label: "Prix",
+                    },
+                    {
+                        cellType: ECellType.PERCENTAGE,
+                        label: "Remise",
+                    },
+                    {
+                        cellType: ECellType.TEXT,
+                        label: "Nom",
+                    },
+                    {
+                        cellType: ECellType.PATATE,
+                        label: "Patate",
+                    },
+                ]}
+                tableData={[
+                    {
+                        Remise: 50,
+                        Nom: "Article",
+                        Prix: 500000000,
+                        Patate: "PATATE",
+                    },
+                    {
+                        Prix: 100,
+                        Remise: 10,
+                        Nom: "Article 2",
+                    },
+                ]}
+            />
+        </PageTemplate>
+    );
 }
